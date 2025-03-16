c0f473ee07cd8054db33052d43d613b6d60e5d64
rsxswp <gamersreza.78@gmail.com>
2025-03-16 04:50:46 -0700

    variable mutable

--- src/main.rs
+++ src/main.rs
@@ -12,3 +12,12 @@ fn test_variable() {
     let name = "Reza Khoirul Wijaya Putra";
     println!("Hello {}", name);
 }
+
+#[test]
+fn test_var_mutable() {
+    let mut name = "Reza Khoirul";
+    println!("Hello {}", name);
+    
+    name = "Abdul";
+    println!("Hello {}", name);
+}
